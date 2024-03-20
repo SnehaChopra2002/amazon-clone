5f38bcad8345acdf268993ac30be7f6e79aef45b
SnehaChopra2002 <sneha.chopra46@gmail.com>
2024-03-20 21:48:21 +0530

    new commit

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,18 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import dotenv from 'dotenv';
-dotenv.config()
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyAUbHO6gH-jhWR1AgUTaWU-Rumt1qACzn0",
+  authDomain: "clone-4b98c.firebaseapp.com",
+  projectId: "clone-4b98c",
+  storageBucket: "clone-4b98c.appspot.com",
+  messagingSenderId: "301215543115",
+  appId: "1:301215543115:web:8004c231d7a0ce358502ce",
+  measurementId: "G-F5ZGYL7G0G"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
